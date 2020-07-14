e6524f1e3fb3ffec25f2db190f87b8fc7e321d19
jesus esis <jesisp17@gmail.com>
2020-07-14 00:42:11 -0300

    cambios proyecto final

--- js/main.js
+++ js/main.js
@@ -13,9 +13,35 @@
             }else{
                 element.style.height = "60px"
             }
-    
         }
-        document.querySelector("#btn-menu").addEventListener("click",onClickButton)
+        document.querySelector("#btn-menu").addEventListener("click",onClickButton);
     }
-    )
+    );
+
+   
+
+    const images = [ 'url(../images/aoe-bg-min.jpeg)', 'url(../images/gta-v-bg-min.jpg)', 'url(../images/fifa-10-bg-min.jpg)', 'url(../images/horizon-bg-min.jpg)', 'url(../images/cod-bg-min.jpg)', 'url(../images/skyrim-bg-min.jpg)', 'url(../images/valhalla-min.jpg)', 'url(../images/silent-hill-min.jpg)', 'url(../images/paladins-min.jpg)', 'url(../images/nba-min.jpg)', 'url(../images/crash-min.jpg)', 'url(../images/injustice-min.jpg)'];
+        
+        var valrandom = null;
+        function change() {
+            
+            
+            var val = Math.floor(Math.random() * 12) + 1;
+        
+            $('#img').fadeOut(1000);
+
+            setTimeout(() => {
+                $('#img').css('background-image', images[val - 1]);
+            }, 1000);
+
+            $('#img').fadeIn(1000);
+
+
+        }
+        $(document).ready(function () {
+            setInterval(() => {
+                change();
+            }, 10000);
+        })
+
     
